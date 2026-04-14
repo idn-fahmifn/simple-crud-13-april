ab28f0f53e59a913fb2337d23f58617c032163c8
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-14 15:06:01 +0700

    2.4

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,16 @@
+@extends('template.app')
+
+@section('title')
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="card-title">
+            <h3>Data Kategori</h3>
+        </div>
+    </div>
+    <div class="col-md-6 d-flex justify-conten-center">
+        <a href="" class="btn btn-succes">Buat Baru</a>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Routing Category
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
 
 // ini khusus untuk statis dan sifatnya semntara
 Route::view('tampilan', 'template.app');
