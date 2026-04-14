9585722c5fe4253e1fea9318ea2be6a99169714f
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-14 16:17:19 +0700

    2.6

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,7 +8,9 @@
             </div>
         </div>
         <div class="col-md-6 d-flex justify-content-end">
-            <a href="" class="btn btn-success">Buat Baru</a>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#showModal">
+                Buat Baru
+            </button>
         </div>
     </div>
 @endsection
@@ -22,6 +24,29 @@
                     <div class="mt-2">Dibuat pada : </div>
                     <a href="" class="btn btn-success mt-2 btn-full d-flex justify-content-center">detail</a>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="showModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Kategori Baru</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="">Nama Kategori</label>
+                            <input type="text" name="nama_kategori" id="nama_kategori" class="form-control mt-2">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
