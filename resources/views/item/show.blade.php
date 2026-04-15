22f91c73caedff8e4e0812dcf36090f61816e11d
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-15 15:10:18 +0700

    3.13

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -87,7 +87,70 @@
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Kategori</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                
+                <form action="{{ route('item.store') }}" enctype="multipart/form-data" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group my-2">
+                            <label for="">Nama barang</label>
+                            <input type="text" name="nama_barang" value="{{ @old('nama_barang') }}" id="nama_barang"
+                                class="form-control mt-2 @error('nama_barang') is-invalid @enderror ">
+                            @error('nama_barang')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group my-2">
+                            <label for="">Kategori barang</label>
+                            <select name="kategori" id=""
+                                class="form-control mt-2 @error('kategori') is-invalid @enderror">
+                                <option value="" disabled>Pilih Kategori</option>
+                                @forelse ($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @empty
+                                    <option value="" disabled>Kategori tidak ditemukan</option>
+                                @endforelse
+                            </select>
+                            @error('stok')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group
+                                my-2">
+                            <label for="">Merek</label>
+                            <input type="text" name="merk" value="{{ @old('merk') }}" id="merk"
+                                class="form-control mt-2 @error('merk') is-invalid @enderror ">
+                            @error('merk')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group my-2">
+                            <label for="">Stok</label>
+                            <input type="number" name="stok" value="{{ @old('stok') }}" id="stok"
+                                class="form-control mt-2 @error('stok') is-invalid @enderror ">
+                            @error('stok')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group my-2">
+                            <label for="">Gambar Barang</label>
+                            <input type="file" name="gambar" value="{{ @old('gambar') }}" id="gambar"
+                                class="form-control mt-2 @error('gambar') is-invalid @enderror" accept="image/*">
+                            @error('gambar')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group my-2">
+                            <label for="">Deskripsi barang</label>
+                            <textarea name="deskripsi" class="form-control mt-2 @error('deskripsi') is-invalid @enderror "></textarea>
+                            @error('deskripsi')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
